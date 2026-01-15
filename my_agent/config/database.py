ddd95c4e6a715d50def9605aa7da62c9d9e56190
Rondi650 <rondinellenunes123@gmail.com>
2026-01-15 18:14:48 -0300

    Refactor database connection setup to use ODBC string for improved robustness and clarity

--- my_agent/config/database.py
+++ my_agent/config/database.py
@@ -1,6 +1,24 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
 from langchain_community.utilities import SQLDatabase
 
-db = SQLDatabase.from_uri(
-    "mssql+pyodbc://localhost/Teste_CallCenter?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes",
+# ODBC string (mais robusto que montar URI na mão)
+connection_string = (
+    "DRIVER={ODBC Driver 17 for SQL Server};"
+    "SERVER=localhost;"
+    "DATABASE=Teste_CallCenter;"
+    "Trusted_Connection=yes;"
+)
+
+connection_url = URL.create(
+    "mssql+pyodbc",
+    query={"odbc_connect": connection_string}
+)
+
+engine = create_engine(connection_url, pool_pre_ping=True)
+
+# Continua usando SQLDatabase (inclusive para Toolkit/Agent depois)
+db = SQLDatabase(
+    engine=engine,
     include_tables=["01 Call-Center-Dataset"]
 )
